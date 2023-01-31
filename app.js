8713d564959dd60594bf7a962ee324384fefba93
harsh patel <harsh.patel@manektech.com>
2023-01-31 16:44:50 +0530

    init

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('service is started...')
+    res.send('service is started....')
 })
 
 const PORT = process.env.PORT || 3000
