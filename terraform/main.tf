c957ffac858262bcf7abff4b31feb9270c01bde9
harsh patel <harsh.patel@manektech.com>
2023-02-01 14:44:52 +0530

    init

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws",
-        version = "~> 4.0"
+      source  = "hashicorp/aws"
+      version = "~>4.0"
     }
   }
   backend "s3" {
@@ -11,82 +11,77 @@ terraform {
 }
 
 provider "aws" {
-   region = "ap-south-1"
+  region = "ap-south-1"
 }
-
-resource "aws_instance" "server" {
-  ami           = "ami-06984ea821ac0a879"
-  instance_type = "t2.micro"
-  key_name = aws_key_pair.deployer.key_name
+resource "aws_instance" "servernode" {
+  ami                    = "ami-06984ea821ac0a879"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.maingroup.id]
-  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+  iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
   connection {
-    type = "ssh"
-    host = "self.public_ip"
-    user = "ubuntu"
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
     private_key = var.private_key
-    timeout = "4m"
+    timeout     = "4m"
   }
-
   tags = {
-    "name": "DeployVM"
+    "name" = "DeployVM"
   }
 }
-
-resource "aws_iam_instance_profile" "ec2_profile" {
+resource "aws_iam_instance_profile" "ec2-profile" {
   name = "ec2-profile"
-  role = "EC2-ECR-AUTH"
+  role = "ECR-LOGIN-AUTO"
 }
-
 resource "aws_security_group" "maingroup" {
   egress = [
     {
-        cidr_blocks = ["0.0.0.0/0"]
-        description = ""
-        from_port = 0,
-        ipv6_cidr_blocks = []
-        prefix_list_ids = []
-        protocol = "-1"
-        security_groups = []
-        self = false
-        to_port = 0
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
     }
-    
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+    }
   ]
 
-  ingress = [
-     {
-        cidr_blocks = ["0.0.0.0/0",]
-        description = ""
-        from_port = 22,
-        ipv6_cidr_blocks = []
-        prefix_list_ids = []
-        protocol = "tcp"
-        security_groups = []
-        self = false
-        to_port = 22
-    },
-     {
-        cidr_blocks = ["0.0.0.0/0"]
-        description = ""
-        from_port = 80,
-        ipv6_cidr_blocks = []
-        prefix_list_ids = []
-        protocol = "tcp"
-        security_groups = []
-        self = false
-        to_port = 80
-    }
-  ]
+
 }
 
 resource "aws_key_pair" "deployer" {
-  key_name = var.key_name
+  key_name   = var.key_name
   public_key = var.public_key
-
 }
 
 output "instance_public_ip" {
-  value = aws_instance.server.public_ip
+  value     = aws_instance.servernode.public_ip
   sensitive = true
 }
