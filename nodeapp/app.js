35b76cd2dce79491b09d6843b8f45e5fb4e17141
harsh patel <harsh.patel@manektech.com>
2023-02-01 12:20:35 +0530

    init

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
     res.send('service is started')
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
     console.log(`Server is listen on port ${PORT}`)
