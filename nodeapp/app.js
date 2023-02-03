752aec389fc782b70993a9a9b1101dd14c208d9b
sachin <sachin.singh@manektech.com>
2023-02-03 17:47:11 +0530

    init

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Service is up and running..");
+  res.send("Service is up and running");
 });
 
 app.listen(8080, () => {
