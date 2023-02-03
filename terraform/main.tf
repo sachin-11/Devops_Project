19ac18db1d8aa9dd533c1b3e665131fb2ed39230
sachin <sachin.singh@manektech.com>
2023-02-03 19:44:32 +0530

    init

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_instance" "servernode" {
   ami                    = "ami-06984ea821ac0a879"
   instance_type          = "t2.micro"
-  key_name               = aws_key_pair.deployer1.key_name
+  key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.maingroup.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
   connection {
@@ -76,7 +76,7 @@ resource "aws_security_group" "maingroup" {
 
 }
 
- resource "aws_key_pair" "deployer1" {
+ resource "aws_key_pair" "deployer" {
   key_name   = var.key_name
   public_key = var.public_key
 }
