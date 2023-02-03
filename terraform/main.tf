7401a323b498597fff930bf4f8768001703bf4cc
sachin <sachin.singh@manektech.com>
2023-02-03 19:21:42 +0530

    init

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,7 +76,7 @@ resource "aws_security_group" "maingroup" {
 
 }
 
-resource "aws_key_pair" "deployer" {
+terraform import resource "aws_key_pair" "deployer" {
   key_name   = var.key_name
   public_key = var.public_key
 }
